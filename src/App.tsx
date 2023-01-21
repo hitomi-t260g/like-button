8190bcd4ec4aaa07840c5035e14d28e073f00fbd
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2023-01-22 03:55:34 +0900

    fix type of onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+
+type onClick =
+  () => void;
+type num = number;
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <LikeButton />
       </header>
     </div>
   );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState<num>(999);
+  const handleClick:onClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+  return <span className="likeButton" onClick={handleClick}>♥ {count}</span>;
 }
 
 export default App;
